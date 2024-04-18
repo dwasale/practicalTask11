a57c9e96c2047a9d24b2bd32b654b2836dadbde5
Rudra <dwasale@gmail.com>
2024-04-18 21:52:14 +0100

    Added a function to check for the latest build

--- definingObjects.js
+++ definingObjects.js
@@ -7,10 +7,10 @@ let car = {
     color: "Grey",
     // Object method
     howOld: function(){
-        return 'The car was build in year ${this.year}';
+        return `The car was build in year ${this.year}`;
     },
 };
-console.table(car);
+console.log(car.howOld());
 
 // Method 2: Using object constructors
 function carDescription(brand, model, year, color){
@@ -23,9 +23,11 @@ function carDescription(brand, model, year, color){
 // Create object instance
 let car1 = new carDescription("Tesla", "Model S", 2024, "Red");
 let car2 = new carDescription("Mercedes", "G-Wagon", 2024, "Grey");
+let car3 = new carDescription("Land ROver", "Valar", 2022, "Gray Green");
+// Created the new transmission and engine type properties which do not exist
 car1["transmission"] = "Automatic";
 car2.engineType = "Electric";
-
+// Note: in this case, either bracket or dot notation can be used to add these properties to the car object
 console.log(car1["transmission"]);
 console.log(car2.engineType);
 // Dot notation
@@ -34,3 +36,18 @@ console.log(car2.model);
 // Bracket[] - Ther properties key is passed as a string enclosed within quotation marks
 console.log(car1["model"]);
 console.log(car2["color"]);
+// Create and array to store car objects
+let cars = [car1, car2, car3];
+
+// Define a function to find most recently manufactured
+function findNewCar(array){
+// Sort the array of cars based on the manufacturing year in descending order
+    array.sort((firstCar, secondCar) =>
+    firstCar.year > secondCar.year ? -1: 1
+);
+// Use template and string literals to dispaly the details
+console.log(`The most recently manufactured car is the ${array[0].brand}
+${array[0].model}, which was made in ${array[0].year}`);
+}
+// Call the findNewCar function
+findNewCar(cars);
