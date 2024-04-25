464f333931b4f2cee89f18fe4220eb4f43c6c510
Rudra <dwasale@gmail.com>
2024-04-25 16:22:20 +0100

    Changes the code to edit and search

--- inventory.js
+++ inventory.js
@@ -15,19 +15,19 @@ let shoe5 = new shoes("Nike", "NK0001", 8, 101);
 // Pushing all items to the array
 let allShoes = [shoe1, shoe2, shoe3, shoe4, shoe5];
 // Function to search any shoe within the array
-function searchShoe(item){
+function searchShoe(product_code){
     // Loop through each item of an shoes array
-    for (let i = 0; i < allShoes.length; i++){
-    // Search item with the name and product_code
-        if (allShoes[i].name === item || allShoes[i].product_code === item){
-            return allShoes[i];
-        } 
+    for (let i in allShoes){
+        if (allShoes[i].product_code == product_code){
+            console.log(`${product_code} item found!`);
+            console.table(allShoes[i]);
+        }
     }
-};
+}
 // Displaying on the console
 console.table(allShoes);
-console.log("--------Table according to the given value---------")
-console.table(searchShoe("Nike"));
+searchShoe("NK0001");
+console.log("----------------------------------------------------------");
 // Function to find the lowest item per value
 // Also it will give the value per item in ascending order/ Which has the less value per item
 function lowestValue(item){
@@ -43,7 +43,7 @@ lowestValue(allShoes);
 // Displaying on the console
 console.log("-----------------Table in ascending order------------------")
 console.table(allShoes);
-console.log("-------------------------------------------------------")
+console.log("\n-------------------------------------------------------")
 // Function to find highest value per item
 // Function to arrange table
 function highestValue(item){
@@ -54,31 +54,30 @@ function highestValue(item){
 console.log(`The item with the high value per item is ${item[0].name}, 
 which value is ${item[0].value_per_item}`);
 }
+// Calling function highestValue as required to see the highest value item
 highestValue(allShoes);
-// Function to eidt all four properties for each of five shoe
-function editProperties(allShoes, item, newItem){
+// Function to eidt the properties for each of five shoe
+function editProperties(product_code, quantity, value_per_item){
+// Storing the index value of the shoe item in a variable through checking the product_code
+    const productIndex = allShoes.findIndex((shoes) => shoes.product_code === product_code);
 // Try catch block if any error occur while running the code below
     try{
-    for (let i = 0; i < allShoes.length; i++){
-// Checking each item according to the given item and replacing with new one
-        if (allShoes[i].name === item){
-            allShoes[i]["name"] = newItem;
-        } else if (allShoes[i].product_code === item) {
-            allShoes[i]["product_code"] = newItem;
-        } else if (allShoes[i].quantity == item) {
-            allShoes[i]["quantity"] = newItem;
-        } else if (allShoes[i].value_per_item == item){
-            allShoes[i]["value_per_item"] = newItem;
-            return allShoes[i];
-        }
-    };
+    // Checking if the index value is not less than -1
+    if (productIndex !== -1){
+        // Changing the shoe quantity and value_per_item as given from the user
+        allShoes[productIndex]["quantity"] = quantity;
+        allShoes[productIndex]["value_per_item"] = value_per_item;
+        console.log(`${product_code} has been edited/updated successfully!`)
+    } else {
+        console.log(`${product_code} item cannot be FOUND!!`);
+    }
 // If any error then it will catches and throw the erro massage 
-} catch (err){
-    console.log(err);
-}
+    } catch (err){
+        console.log(err);
+    }
 }
 //calling the editProperties functions with the older value to be replaced with new value
-editProperties(allShoes, 6, 7);
-console.log("\nThe updated table: ");
+editProperties("CKL23", 5, 10);
+// console.log("The updated table: ");
 // Prints the array in table format on the console
 console.table(allShoes);
